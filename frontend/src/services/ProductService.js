db16ed3ef0e7e3fffd9b097c33adf960489e208c
soufiane fatih <fatihe.soufiane@gmail.com>
2022-04-20 10:47:58 +0000

    add create  product service

--- frontend/src/services/ProductService.js
+++ frontend/src/services/ProductService.js
@@ -9,3 +9,21 @@ axios.defaults.headers = {
 export function getAllproduct() {
   return axios.get(APP_URL + "product");
 }
+
+export function createProduct(name,decsription,price, image ) {
+    console.log("getAllcategory");
+  
+    const formData = new FormData();
+  
+    formData.append("image", image, image.name);
+  
+    formData.append("name", name);
+    formData.append("decsription", decsription);
+    formData.append("price", price);
+
+
+  
+    console.log("formData : ", formData.get("image"));
+  
+    return axios.post(APP_URL + "product/create", formData);
+  }
